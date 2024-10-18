39fc701c0a32843ab5a17f7bf842c47108df2673
Jacks Coding Plan <jack28smit@gmail.com>
2024-10-18 11:50:51 +0200

    Add files via upload

---
+++ MarsWeigh.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  // Add your code below  
+double weight;
+double weightMars = weight/3;
+
+
+std::cin >> weight;
+std::cout << "This Item would weigh:" << weightMars << " on mars.\n";
+
+}
